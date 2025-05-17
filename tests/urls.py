1ca6940b65b51fd04c87413f437721156afa9867
Michael Zehetleitner <michael.zehetleitner@ku.de>
2025-05-17 13:28:12 +0200

    chore: remove leftover references to pruned apps

--- tests/urls.py
+++ tests/urls.py
@@ -3,14 +3,9 @@ from django.urls import include, path
 from fragdenstaat_de.theme.urls import urlpatterns as base_urlpatterns
 
 urlpatterns = [
-    path("blog/", include("fragdenstaat_de.fds_blog.urls", namespace="blog")),
     path("cms/search/", include("fragdenstaat_de.fds_cms.urls", namespace="fds_cms")),
     path(
         "cms/contact/",
         include("fragdenstaat_de.fds_cms.contact", namespace="fds_cms_contact"),
-    ),
-    path(
-        "spenden/spende/",
-        include("fragdenstaat_de.fds_donation.urls", namespace="fds_donation"),
     ),
 ] + base_urlpatterns
